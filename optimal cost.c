535278de42976fa56ff9216bed40efddb3c74765
Bhargav0780 <113417255+Bhargav0780@users.noreply.github.com>
2023-11-14 09:53:54 +0530

    Update optimal cost.c

--- optimal cost.c
+++ optimal cost.c
@@ -1,30 +1,39 @@
-#include <iostream>
-using namespace std;
-int min_(int a, int b, int c){
-   if (a < b)
-      return (a < c) ? a : c;
-   else
-      return (b < c) ? b : c;
+#include <stdio.h>
+
+#define MAX_ITEMS 100
+#define MAX_CAPACITY 100
+
+int max(int a, int b) {
+    return (a > b) ? a : b;
 }
-int min_cost(int cost[4][4], int m, int n){
-   int i, j;
-   int tot_cost[4][4];
-   tot_cost[0][0] = cost[0][0];
-   for (i = 1; i <= m; i++)
-   tot_cost[i][0] = tot_cost[i - 1][0] + cost[i][0];
-   for (j = 1; j <= n; j++)
-      tot_cost[0][j] = tot_cost[0][j - 1] + cost[0][j];
-   for (i = 1; i <= m; i++)
-      for (j = 1; j <= n; j++)
-         tot_cost[i][j] = min_(tot_cost[i - 1][j - 1], tot_cost[i - 1][j], tot_cost[i][j - 1]) + cost[i][j];
-      return tot_cost[m][n];
+
+int knapsack(int values[], int weights[], int n, int capacity) {
+    int dp[MAX_ITEMS + 1][MAX_CAPACITY + 1];
+
+    // Initialize the memoization table
+    for (int i = 0; i <= n; i++) {
+        for (int w = 0; w <= capacity; w++) {
+            if (i == 0 || w == 0)
+                dp[i][w] = 0;
+            else if (weights[i - 1] <= w)
+                dp[i][w] = max(values[i - 1] + dp[i - 1][w - weights[i - 1]], dp[i - 1][w]);
+            else
+                dp[i][w] = dp[i - 1][w];
+        }
+    }
+
+    return dp[n][capacity];
 }
-int main(){
-   int cost[4][4] = {
-      { 9, 9, 4 },
-      { 8, 0, 9 },
-      {1, 2, 8}
-   };
-   cout<<" The minimum cost is "<<min_cost(cost, 2, 2);
-   return 0;
+
+int main() {
+    int values[] = {60, 100, 120};
+    int weights[] = {10, 20, 30};
+    int n = sizeof(values) / sizeof(values[0]);
+    int capacity = 50;
+
+    int optimalValue = knapsack(values, weights, n, capacity);
+
+    printf("Optimal Value: %d\n", optimalValue);
+
+    return 0;
 }
